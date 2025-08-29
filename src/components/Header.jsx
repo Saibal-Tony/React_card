c7c76ee140245d4631f834337d9ab4e1ccad169a
Saibal Bera <berasaibal20@gmail.com>
2025-08-29 12:33:57 +0530

    some changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,13 @@ import logo from "../assets/react.svg"
 export default function Header() 
 { //no name needed here in default export
     return (
-        <header className='header'>
-            <img src={logo} className='nav-img' alt='react logo' />
-            <span>
-                React Facts
-            </span>
+        <header>
+            <nav>
+                <img src={logo} className='nav-img' alt='react logo' />
+                <span>
+                    React Facts
+                </span>
+            </nav>
         </header>
     )
 }
