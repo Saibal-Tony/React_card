0927d56fc48bbd868e7a141b03de2dd4566a1da5
Saibal Bera <berasaibal20@gmail.com>
2025-09-02 13:00:54 +0530

    some changed

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,17 @@
+import logo from "../assets/react.svg";
+
 export default function Main() {
     return (
-        <main className="main"> 
-            <p>Fun facts about react</p>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100K on Github</li>
-            <li>Is mentained by meta</li>
-            <li>Powers thousands of enterprise app, including mobile apps</li>
-        </ul> 
+        <main> 
+            {/* <img src={logo} alt="React logo" className="react-logo" /> */}
+            <h1>Fun facts about react</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K on Github</li>
+                <li>Is mentained by meta</li>
+                <li>Powers thousands of enterprise app, including mobile apps</li>
+            </ul> 
         </main>
     )
 }
